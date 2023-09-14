9c4d9e3df5bedab55157f4148ccf5f257310ce98
krishnamk26 <krissurya1@gmail.com>
2023-09-14 23:30:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "react-notifications/lib/notifications.css"
+import { App } from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
